4ecb55ec53687673c688410ea945084d082ebfe5
Joao Victor Souza <joao.victor.souza99@hotmail.com>
2018-10-29 05:39:02 -0300

    Mudança de Layout e Programação

--- htdocs/js/controllersAngular.js
+++ htdocs/js/controllersAngular.js
@@ -4,9 +4,15 @@ String.prototype.capitalizeFirstLetter = function() {
 
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    $scope.nome = "João";
-    $scope.sobrenome = "Souza";
-    $scope.exibe = function(nome){
-        return nome ? true : false;
+    $scope.name = "Hello";
+    $scope.sobrenome = "World";
+    $scope.exibe = function(name, sobrenome){
+       if(name && sobrenome) return true;
+       else return false;
     };
+    $scope.limpar = () => {
+        $scope.name = null;
+        $scope.sobrenome = null;
+        document.getElementById("txtnome").focus();
+    }
 });
